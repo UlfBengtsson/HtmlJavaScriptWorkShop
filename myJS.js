3f54e195cd0dd91baee7cd3c5ce55be1607f4e1f
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-11-25 13:59:28 +0100

    before using addlisenerEvent

--- myJS.js
+++ myJS.js
@@ -1,4 +1,12 @@
 var myH1 = document.getElementById("changeMe");
 
-myH1.innerHTML = "Hello world wide web!";
+myH1.innerHTML = "<b>Hello world wide web!</b>";
 myH1.style.color = "Blue";
+
+var myUlElements = document.getElementsByTagName("ul");
+
+console.log(myUlElements);
+
+function giveAnimals() {
+  myUlElements[0].innerHTML = "<li>Dog</li><li>Cat</li>";
+}
