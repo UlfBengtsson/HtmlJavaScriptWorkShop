561882412ef310cc992078a28bf795a73102bd10
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-11-25 14:30:58 +0100

    used AddEventListener to btn and h2

--- myJS.js
+++ myJS.js
@@ -7,6 +7,18 @@ var myUlElements = document.getElementsByTagName("ul");
 
 console.log(myUlElements);
 
+var btnDont = document.getElementById("btnDont");
+
+btnDont.addEventListener("click", giveAnimals);
+
 function giveAnimals() {
   myUlElements[0].innerHTML = "<li>Dog</li><li>Cat</li>";
 }
+
+document.getElementById("secret").addEventListener("mouseover", function() {
+  document.getElementsByTagName("h2")[0].innerHTML =
+    "This is a eventListener reacting to you";
+});
+document.getElementById("secret").addEventListener("mouseleave", function() {
+  document.getElementsByTagName("h2")[0].innerHTML = "Secrets under this text";
+});
